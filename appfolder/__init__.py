09bc5e73aed13b72f074c8d54730e3877109df24
thomas <thomas@xal.no>
2019-09-09 14:44:43 +0200

    version with login-form.

--- appfolder/__init__.py
+++ appfolder/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from config import Config
 
 demoapp = Flask(__name__)
+demoapp.config.from_object(Config)
 
 from appfolder import routes
 
--- appfolder/routes.py
+++ appfolder/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, redirect, flash, url_for
 from appfolder import demoapp
+from appfolder.forms import LoginForm
 
 @demoapp.route('/')
 @demoapp.route('/index')
@@ -20,3 +21,13 @@ def index():
         title='Title for the page', 
         user=user,
         posts=posts)
+
+
+@demoapp.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title="Sign in", form=form)
