2a5e76c974c0e86d214657993aa045c054ad262a
thomas <thomas@xal.no>
2019-09-09 12:02:42 +0200

    version after part2 of tutorial

--- appfolder/routes.py
+++ appfolder/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from appfolder import demoapp
 
 @demoapp.route('/')
 @demoapp.route('/index')
 def index():
-    return "in indexfunction."
+    user = {'username': 'thomas'}
+    posts = [
+        {
+            'author': {'username': 'kjell'},
+            'body': 'the book animal farm by george orwell',
+        },
+        {
+            'author': {'username': 'randi'},
+            'body': 'considering a liberal vote next time...'
+        }
+    ]
+
+    return render_template('index.html', 
+        title='Title for the page', 
+        user=user,
+        posts=posts)
