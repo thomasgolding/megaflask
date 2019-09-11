5b846f5546a49f18a1e6a09a6f317387b2a6b20d
thomas <thomas@xal.no>
2019-09-11 13:39:16 +0200

    now with login and user-creation.

--- appfolder/__init__.py
+++ appfolder/__init__.py
@@ -2,15 +2,17 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate 
-
-
+from flask_login import LoginManager
 
 demoapp = Flask(__name__)
 demoapp.config.from_object(Config)
 
 db = SQLAlchemy(demoapp)
 migrate = Migrate(demoapp, db)
+login = LoginManager(demoapp)
 
+## define the endpoint for logging in.
+login.login_view = 'login'
 
 from appfolder import routes, models
 
--- appfolder/routes.py
+++ appfolder/routes.py
@@ -1,11 +1,18 @@
-from flask import render_template, redirect, flash, url_for
-from appfolder import demoapp
-from appfolder.forms import LoginForm
+from flask import render_template, redirect, flash, url_for, request
+from flask_login import current_user, login_user, login_required, logout_user
+from werkzeug.urls import url_parse
+
+from appfolder import demoapp, db
+from appfolder.forms import LoginForm, RegistrationForm
+from appfolder.models import User
+
 
 @demoapp.route('/')
 @demoapp.route('/index')
+@login_required
 def index():
-    user = {'username': 'thomas'}
+    #user = {'username': 'thomas'}
+    user = current_user
     posts = [
         {
             'author': {'username': 'kjell'},
@@ -25,9 +32,56 @@ def index():
 
 @demoapp.route('/login', methods = ['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember me={}'.format(
-            form.username.data, form.remember_me.data))
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
+    
+    form = LoginForm()
+
+    ## this happens if the submitbutton is hit. 
+    if form.validate_on_submit():
+        
+        ## get the user
+        user = User.query.filter_by(username=form.username.data).first()
+        
+        ## check if user-pw does not exist
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password.')
+            return redirect(url_for('login'))
+        
+        ## user-pw dows exist, so now the user is logged in.
+        login_user(user, remember=form.remember_me.data)
+        ## since we are using login_requested we are getting a potential argument in the url string:
+        ## /login?next=/index. 
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        
+        return redirect(next_page)
+
     return render_template('login.html', title="Sign in", form=form)
+
+@demoapp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+
+@demoapp.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        redirect(url_for('index'))
+    
+    ## fill form
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username= form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("User created.")
+        return redirect(url_for('login'))
+
+
+
+    return render_template('register.html', title='Register', form=form)
